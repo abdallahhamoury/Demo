45199b224c8cf052d97dacf0d7ff01725e9e060e
abdallahhamoury <>
2021-06-05 15:54:51 +0000

    new

---
+++ script.js
@@ -0,0 +1,41 @@
+
+var username;
+username = "abdallah";
+var myid = 1234;
+var test = true;
+
+
+
+console.log(username);
+
+
+alert("welcom to our webpage " + username)
+
+
+document.write(" <p style='color:red;' > this is " + username + " from js </p> ")
+
+var x = "5";
+var y = 5;
+
+console.log(5 ** 2);
+// console.log(y++);
+console.log(y);
+console.log(y == x);
+console.log(y === x)
+console.log(y != x)
+
+var userage =  prompt(" Enter your age? ")
+console.log(userage);
+
+var age = 25;
+
+if (age <= 25) {
+  alert("hello")
+} else if (age > 25) {
+  alert("hey")
+} else {
+  alert("hi")
+}
+
+
+
